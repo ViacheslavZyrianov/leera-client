958f49dc6715db75581c94649cbd9d1227046824
ViacheslavZyrianov <viacheslavz.dev@support.team>
2023-12-04 17:00:21 +0200

    remove homepage link

--- components/header-bar/links.ts
+++ components/header-bar/links.ts
@@ -1,10 +1,5 @@
 export default [
     {
-        to: '/',
-        label: 'home',
-        icon: 'mdi-home'
-    },
-    {
         to: '/stories',
         label: 'stories',
         icon: 'mdi-format-list-text'
