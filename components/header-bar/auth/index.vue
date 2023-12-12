3575acc19b205a68b9de46b31a0f8f9b805ca21a
ViacheslavZyrianov <viacheslavz.dev@support.team>
2023-12-12 22:35:34 +0200

    fix header

--- components/header-bar/auth/index.vue
+++ components/header-bar/auth/index.vue
@@ -1,31 +1,34 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
 const localePath = useLocalePath()
-
-const { username } = userStore.me
-
-const avatarInitials = username[0]
 </script>
 
 <template>
   <v-btn
     v-if="checkIsLoggedIn()"
+    variant="outlined"
+    class="ml-4"
     :to="localePath('/profile')"
-    icon
-    variant="outlined"
-    size="small"
-    class="text-uppercase avatar-initial"
-    :text="avatarInitials"
-  />
-  <v-btn
-    v-else
-    :to="localePath('/auth')"
-    icon="mdi-account"
-    variant="outlined"
-    size="small"
-  />
+  >
+    {{ $t('profile.label') }}
+  </v-btn>
+  <template v-else>
+    <v-btn
+      variant="outlined"
+      class="ml-4"
+      :to="localePath('/login')"
+    >
+      {{ $t('login.label') }}
+    </v-btn>
+    <v-btn
+      dark
+      variant="flat"
+      class="ml-4"
+      color="secondary"
+      :to="localePath('/registration')"
+    >
+      {{ $t('registration.label') }}
+    </v-btn>
+  </template>
 </template>
 
 <style scoped>
